e8d726fa95a73e91038daebbf0b132f91b2995be
Clément <lafargue.pro@gmail.com>
2021-09-06 16:48:48 +0200

    index before responsive

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,16 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
-
     $('#header').load('./front/layouts/header.html');
+    $('#footer').load('./front/layouts/footer.html');
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500) {
+            $(".backtotop").removeClass("hidden").addClass('visible');
+            $('.navbar').addClass('fixedtop');
+        } else {
+            $(".backtotop").removeClass("visible").addClass('hidden');
+            $('.navbar').removeClass('fixedtop');
+        }
+    });
 });
